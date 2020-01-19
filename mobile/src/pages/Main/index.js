418f3e59ad19dac4fd8e0a31ececce1fa689006f
Yago Theodoro <freonzx@gmail.com>
2020-01-19 04:26:03 -0300

    Mobile: Add Main API request

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -7,6 +7,7 @@ import {
 import { Marker, Callout } from 'react-native-maps';
 import { Image } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import api from '../../services/api';
 import {
   Map,
   DevView,
@@ -20,6 +21,8 @@ import {
 
 export default function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
+  const [devs, setDevs] = useState([]);
+  const [techs, setTechs] = useState('');
 
   useEffect(() => {
     async function loadInitialPosition() {
@@ -48,42 +51,72 @@ export default function Main({ navigation }) {
     return null;
   }
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get('/search', {
+      params: {
+        latitude,
+        longitude,
+        techs,
+      },
+    });
+
+    setDevs(response.data);
+  }
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
+
   return (
     <>
-      <Map initialRegion={currentRegion}>
-        <Marker coordinate={{ latitude: -23.489354, longitude: -46.750816 }}>
-          <Image
-            style={{
-              width: 54,
-              height: 54,
-              borderRadius: 4,
-              borderWidth: 4,
-              borderColor: '#fff',
-            }}
-            source={{
-              uri:
-                'https://avatars0.githubusercontent.com/u/26206406?s=460&v=4',
-            }}
-          />
-
-          <Callout
-            onPress={() => {
-              navigation.navigate('Profile', { github_username: 'freonzx' });
+      <Map
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+      >
+        {devs.map(dev => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              latitude: dev.location.coordinates[1],
+              longitude: dev.location.coordinates[0],
             }}
           >
-            <DevView>
-              <DevName>Yago Theodoro</DevName>
-              <DevBio>Fullstack Developer</DevBio>
-              <DevTechs>ReactJs, NodeJs</DevTechs>
-            </DevView>
-          </Callout>
-        </Marker>
+            <Image
+              style={{
+                width: 54,
+                height: 54,
+                borderRadius: 4,
+                borderWidth: 4,
+                borderColor: '#fff',
+              }}
+              source={{
+                uri: dev.avatar_url,
+              }}
+            />
+
+            <Callout
+              onPress={() => {
+                navigation.navigate('Profile', {
+                  github_username: dev.github_username,
+                });
+              }}
+            >
+              <DevView>
+                <DevName>{dev.name}</DevName>
+                <DevBio>{dev.bio}</DevBio>
+                <DevTechs>{dev.techs.join(', ')}</DevTechs>
+              </DevView>
+            </Callout>
+          </Marker>
+        ))}
       </Map>
 
       <InputView>
-        <SearchInput />
+        <SearchInput value={techs} onChangeText={setTechs} />
 
-        <SearchBtn>
+        <SearchBtn onPress={loadDevs}>
           <MaterialIcons
             onPress={() => {}}
             name='my-location'
