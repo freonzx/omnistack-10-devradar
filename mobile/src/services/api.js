050056fd4bce0a7136c80f30871f3cdf112e6aac
Yago Theodoro <freonzx@gmail.com>
2020-01-20 21:32:22 -0300

    Mobile: Change API URL service and app name

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.15:3333',
+  baseURL: 'https://blooming-wildwood-40548.herokuapp.com/',
 });
 
 export default api;
